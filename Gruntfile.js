cb62d50d5bb5517b784f805d90414fba7083ebd7
term96 <term96@yandex.com>
2017-03-25 18:39:51 +0300

    Now using ReactJS

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,13 +11,32 @@ module.exports = function(grunt) {
         src: ['.build/']
       }
     },
+    babel: {
+      options: {
+        plugins: ['transform-react-jsx'],
+        presets: ['es2015', 'react']
+      },
+      jsx: {
+        files: [{
+          expand: true,
+          cwd: 'site/scripts/jsx', // Custom folder
+          src: ['*'],
+          dest: 'dist/js', // Custom folder
+          ext: '.js'
+        }]
+      }
+    },
     concat: {
-      js: {
-        src: ['site/js/*'],
+      scripts: {
+        src: ['site/scripts/js/*.js'],
+        dest: 'dist/js/scripts.js'
+      },
+      react: {
+        src: ['site/scripts/react/react.js', 'site/scripts/react/react-dom.js', 'dist/js/app.js', 'dist/js/scripts.js'],
         dest: 'dist/js/scripts.js'
       },
       css: {
-        src: ['node_modules/bootstrap/dist/css/bootstrap.min.css', 'site/css/*'],
+        src: ['site/css/*'],
         dest: 'dist/css/styles.css'
       }
     },
@@ -86,15 +105,19 @@ module.exports = function(grunt) {
         livereload: true
       },
       js: {
-        files: 'site/**/*.js',
+        files: 'site/scripts/js/*',
         tasks: ['build-js']
       },
+      jsx: {
+        files: 'site/scripts/jsx/*',
+        tasks: ['babel', 'build-js']
+      },
       css: {
-        files: 'site/**/*.css',
+        files: 'site/css/*',
         tasks: ['default']
       },
       html: {
-        files: 'site/**/*.html',
+        files: 'site/index.html',
         tasks: ['build-html']
       },
       build: {
@@ -104,6 +127,7 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-babel');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
@@ -113,9 +137,9 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-connect');
 
-  grunt.registerTask('default', ['clean', 'concat', 'uglify', 'cssmin', 'eslint', 'copy', 'hashres']);
+  grunt.registerTask('default', ['clean', 'babel', 'concat:scripts', 'uglify', 'concat:css', 'cssmin', 'eslint', 'concat:react', 'copy', 'hashres']);
   grunt.registerTask('build-css', ['clean:build', 'concat:css', 'cssmin', 'copy', 'hashres']);
-  grunt.registerTask('build-js', ['clean:build', 'concat:js', 'uglify', 'eslint', 'copy', 'hashres']);
+  grunt.registerTask('build-js', ['clean:build', 'concat:scripts', 'uglify', 'eslint', 'concat:react', 'copy', 'hashres']);
   grunt.registerTask('build-html', ['clean:build', 'copy', 'hashres']);
   grunt.registerTask('serve', ['connect', 'watch']);
 
